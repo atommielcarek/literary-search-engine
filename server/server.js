9b0f409b5c098016f3f13ed0b728efdcba03b995
Prometheus <atommielcarek@gmail.com>
2021-10-31 15:27:19 -0600

    deploying to heroku...again

--- server/server.js
+++ server/server.js
@@ -1,30 +1,25 @@
 const express = require('express');
 const path = require('path');
-// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
-
-// import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
-
+const { typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
 
-const PORT = process.env.PORT || 3001;
+const db = require('./config/connection');
+
 const app = express();
-// create a new Apollo server and pass in our schema data
+const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context:authMiddleware
+  context: authMiddleware
 });
 
-// integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -36,7 +31,6 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
